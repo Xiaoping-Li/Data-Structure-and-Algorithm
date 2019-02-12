7cf52443e742ad2f9aeb515af7ff29529352cb24
Xiaoping-Li <narci2008@gmail.com>
2019-02-12 14:23:43 -0800

    Update SelectionSort.js

--- Sorts/SelectionSort.js
+++ Sorts/SelectionSort.js
@@ -17,3 +17,30 @@ small amount of extra storage space is allowed for auxiliary variables. The inpu
 algorithm executes. In-place algorithm updates input sequence only through replacement or swapping of elements. An algorithm 
 which is not in-place is sometimes called not-in-place or out-of-place.
 */
+
+const selectSort = (numList) => {
+  // Return numList directly if array has less than 2 elements
+	if (numList.length < 2) return numList;
+  
+  // Outside loop: from 0 to the second last element
+	for (let i = 0; i < numList.length - 1; i++) {
+		let min = numList[i];
+		let minIdx = i;
+
+		// Find the min value and index from unsorted sublist
+		for (let j = i + 1; j < numList.length; j++) {
+			if (numList[j] < min) {
+				min = numList[j];
+				minIdx = j;
+      }
+    }
+
+    // Swap the min value with the most left element of unsorted sublist
+    numList[minIdx] = numList[i];
+    numList[i] = min; 
+  }
+  return numList;
+};
+
+const array = [6, 5, 2, 1, 3, 0];
+console.log(selectSort(array));
