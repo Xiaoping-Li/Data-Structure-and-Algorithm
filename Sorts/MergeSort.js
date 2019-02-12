3c4264841b93e7e834f0fcbd3d45e274547585d2
Xiaoping-Li <narci2008@gmail.com>
2019-02-12 15:39:33 -0800

    Update MergeSort.js

--- Sorts/MergeSort.js
+++ Sorts/MergeSort.js
@@ -3,6 +3,14 @@ Merge Sort
 
 In computer science, merge sort (also commonly spelled mergesort) is an efficient, general-purpose, comparison-based sorting 
 algorithm. Most implementations produce a stable sort, which means that the order of equal elements is the same in the input 
-and output. Merge sort is a divide and conquer algorithm
+and output. Merge sort is a divide and conquer algorithm.
+
+Algorithm
+Merge sort keeps on dividing the list into equal halves until it can no more be divided. By definition, if it is only one 
+element in the list, it is sorted. Then, merge sort combines the smaller sorted lists keeping the new list sorted too.
+
+Step 1 − if it is only one element in the list it is already sorted, return.
+Step 2 − divide the list recursively into two halves until it can no more be divided.
+Step 3 − merge the smaller lists into new list in sorted order.
 
 */
