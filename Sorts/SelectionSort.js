c41bdb86782060b82430c1855ed6f5d035127559
Xiaoping-Li <narci2008@gmail.com>
2019-02-12 14:00:03 -0800

    Update SelectionSort.js

--- Sorts/SelectionSort.js
+++ Sorts/SelectionSort.js
@@ -1,12 +1,19 @@
 /*
 Selection Sort
 
-The selection sort algorithm sorts an array by repeatedly finding the minimum element (considering ascending order) from 
-unsorted part and putting it at the beginning. The algorithm maintains two subarrays in a given array.
+In computer science, selection sort is a sorting algorithm, specifically an in-place comparison sort. It has O(n2) time 
+complexity, making it inefficient on large lists, and generally performs worse than the similar insertion sort. Selection sort 
+is noted for its simplicity, and it has performance advantages over more complicated algorithms in certain situations, 
+particularly where auxiliary memory is limited.
 
-1) The subarray which is already sorted.
-2) Remaining subarray which is unsorted.
+The algorithm divides the input list into two parts: the sublist of items already sorted, which is built up from left to right 
+at the front (left) of the list, and the sublist of items remaining to be sorted that occupy the rest of the list. Initially, 
+the sorted sublist is empty and the unsorted sublist is the entire input list. The algorithm proceeds by finding the smallest 
+(or largest, depending on sorting order) element in the unsorted sublist, exchanging (swapping) it with the leftmost unsorted 
+element (putting it in sorted order), and moving the sublist boundaries one element to the right.
 
-In every iteration of selection sort, the minimum element (considering ascending order) from the unsorted subarray is picked 
-and moved to the sorted subarray.
+***In computer science, an in-place algorithm is an algorithm which transforms input using no auxiliary data structure. However a 
+small amount of extra storage space is allowed for auxiliary variables. The input is usually overwritten by the output as the 
+algorithm executes. In-place algorithm updates input sequence only through replacement or swapping of elements. An algorithm 
+which is not in-place is sometimes called not-in-place or out-of-place.
 */
