47e3aeb6a3af6317e1482e01dd7edd6b60600967
Xiaoping-Li <narci2008@gmail.com>
2019-02-11 16:08:24 -0800

    Update InsertionSort.js

--- Sorts/InsertionSort.js
+++ Sorts/InsertionSort.js
@@ -1,4 +1,8 @@
-/*1. 插入排序分为直接插入排序，二分插入排序，希尔排序。其中直接插入排序和二分插入排序是稳定的算法，希尔排序是不稳定的算法。
+/*
+1. 插入排序分为直接插入排序，二分插入排序，希尔排序。其中直接插入排序和二分插入排序是稳定的算法，希尔排序是不稳定的算法。
+2. Insertion sort iterates, consuming one input element each repetition, and growing a sorted output list. At each iteration, 
+insertion sort removes one element from the input data, finds the location it belongs within the sorted list, and inserts it 
+there. It repeats until no input elements remain.
 
 
 */
