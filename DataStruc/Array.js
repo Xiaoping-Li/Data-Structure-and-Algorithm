a954c138897fec324f2ab0def3bbc99616808762
Xiaoping-Li <narci2008@gmail.com>
2019-02-12 12:06:36 -0800

    Update Array.js

--- DataStruc/Array.js
+++ DataStruc/Array.js
@@ -15,10 +15,28 @@ class myArray {
 
   clearList() {
     this._array = [];
+    this._length = 0;
   }
 
   insert(idx, ele) {
+    if (idx < 0 || idx > this._length) {
+      return this._length === 0 ? `List is empty` : `idx is invalid! Provide idx between 0 - ${this._length}`;
+    }
 
+    if (this._length === idx) {
+      this._array[idx] = ele;
+      this._length++;
+      return this._array[idx];
+    }
+     
+    let swapIdx = this._length - 1;
+    while (swapIdx >= idx) {
+      this._array[swapIdx + 1] = this._array[swapIdx];
+      swapIdx--;
+    }
+    this._array[idx] = ele;
+    this._length++;
+    return this._array[idx];
   }
 
   getElem(idx) {
@@ -35,3 +53,15 @@ class myArray {
   }
 
 }
+
+const newArray = new myArray();
+
+newArray.insert(0, 'a');
+newArray.insert(1, 'b');
+newArray.insert(2, 'd');
+newArray.insert(3, 'e');
+newArray.insert(4, 'f');
+newArray.insert(5, 'g');
+console.log(newArray);
+newArray.insert(2, 'c');
+console.log(newArray);
