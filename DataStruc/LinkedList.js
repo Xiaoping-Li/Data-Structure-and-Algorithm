80b0e2728009dd636f0bcfb3f1ccfe9a2f1ea355
Xiaoping-Li <narci2008@gmail.com>
2019-02-13 13:01:09 -0800

    Update LinkedList.js

--- DataStruc/LinkedList.js
+++ DataStruc/LinkedList.js
@@ -2,3 +2,39 @@
 Linked List
 
 */
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this._length = 0;
+  }
+  
+  isEmpty() {
+    return !this._length ? true : false;
+  }
+  
+  getElem(i) {
+    if (i < 0 || i > this._length - 1) return 'Invalid i';
+    let j = 0;
+    let current = this.head;
+    while (j < i) {
+      current = current.next;
+      j++;
+    }
+    return current.value;
+  }
+  
+  contain(value) {
+    if (!this.head) return false;
+    let i = 0;
+    let current = this.head;
+    while (current) {
+      if (current.value === value) return i;
+      current = current.next;
+      i++;
+    }
+    return false;
+  }
+  
+  
+}
