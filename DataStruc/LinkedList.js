51c95f43998a91a937e55fcf5ff3c71ae69079f6
Xiaoping-Li <narci2008@gmail.com>
2019-02-14 15:04:55 -0800

    Update LinkedList.js

--- DataStruc/LinkedList.js
+++ DataStruc/LinkedList.js
@@ -52,27 +52,45 @@ class LinkedList {
   
   insert(array, i) {
     if (i < 0 || i > this._length) return 'Invalid i';
+
+    const headNode = {
+      value: array[0],
+      next: null,
+    };
     
+    // Empty Linked List and add items to index 0
     if (!this.head && i === 0) {
-      this.head = {
-        value: array[0],
-        next: null,
-      };
+      this.head = headNode;
 
       let current = this.head;
       let arraySlice = array.slice(1);
-      insertArray(current, arraySlice); // Use helper function
+      insertArray(current, arraySlice);
 
       this._length += array.length;
       return this.head;
     }
-        
+    
+    // Add items to the beginning of a nonempty Linked List
+    if (this.head && i === 0) {
+      let firstNode = this.head;
+      this.head = headNode;
+
+      let current = this.head;
+      let arraySlice = array.slice(1);
+      current = insertArray(current, arraySlice);
+      current.next = firstNode;
+
+      this._length += array.length;
+      return this.head;
+    }
+    
+    // Add items to a nonempty linked list from anywhere except the 0 index   
     let j = 0; 
     let current = this.head;
     while (current) {
       if (j === i - 1) {
         let nextNode = current.next;
-        current = insertArray(current, array); // Use helper function
+        current = insertArray(current, array);
         current.next = nextNode;
 
         this._length += array.length;
@@ -83,5 +101,11 @@ class LinkedList {
       }
     }
   } 
+  
+  removeItem(i) {
+    if (i < 0 || i > this._length - 1) return 'Invalide index';
+    
+    
+  }
        
 }
