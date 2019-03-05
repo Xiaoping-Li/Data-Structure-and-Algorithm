d4301acf22a50659f7624448baa5d49e4a236f94
Xiaoping-Li <narci2008@gmail.com>
2019-03-05 10:40:27 -0800

    Update KMP.js

--- DataStruc/KMP.js
+++ DataStruc/KMP.js
@@ -1,1 +1,10 @@
+// Find array Next first: Longest proper prefix which is also suffix
+function findNext(pat) {
+  const lps = [];
+     
+}
 
+// Searching Algorithm
+function findPattern(str) {
+  
+}
