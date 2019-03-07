1e5a381f4befa56191c5dc070b8f0d6701424f99
Xiaoping-Li <narci2008@gmail.com>
2019-03-07 11:44:54 -0800

    Update Binary Tree.js

--- DataStruc/Binary Tree.js
+++ DataStruc/Binary Tree.js
@@ -1,1 +1,24 @@
+// Define tree Node
+function Node(data) {
+  this.data = data;
+  this.left = null;
+  this.right = null;
+}
 
+// Define Binary Tree class
+class BinaryTree {
+  constructor() {
+    this.root = null;
+  }
+  
+  insert(data) {
+    
+  }
+  
+  inOrderTraversal() {}
+  
+  preOrderTraversal() {}
+  
+  postOrderTraversal() {}
+  
+}
