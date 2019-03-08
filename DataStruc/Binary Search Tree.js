a1441d475a5985a1db2343223c78e26fc8e5a4de
Xiaoping-Li <narci2008@gmail.com>
2019-03-08 09:52:43 -0800

    Update Binary Search Tree.js

--- DataStruc/Binary Search Tree.js
+++ DataStruc/Binary Search Tree.js
@@ -1,1 +1,41 @@
+// Define "Node" class
+function Node(data) {
+  this.data = data;
+  this.left = null;
+  this.right = null;
+}
 
+// Define Binary-Search-Tree Class
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  
+  insert(value) {
+    const newNode = new Node(value);
+    if (!this.root) {
+       this.root = newNode;
+    } else {
+      if (value > this.root.data) {
+        if (!this.right) {
+          this.right = newNode;
+        } else {
+          this.right.insert(value);
+        }
+      } else {
+        if (!this.left) {
+          this.left = newNode;
+        } else {
+          this.left.insert(value)
+        }
+      }
+    }
+    return this.root;
+  }
+  
+  // inOrderTraversal() {}
+  
+  // preOrderTraversal() {}
+  
+  // postOrderTraversal() {}
+}
