fc40910db777ef855c1e402c6f1b08eb8fef2391
Xiaoping-Li <narci2008@gmail.com>
2019-03-28 16:15:42 -0700

    Create Binary Heap.js

---
+++ DataStruc/Binary Heap.js
@@ -0,0 +1,11 @@
+/*
+Binary Heap
+1). The root element will be at Arr[0].
+2). Below table shows indexes of other nodes for the ith node, i.e., Arr[i]:
+      Arr[(i-1)/2]	Returns the parent node
+      Arr[(2*i)+1]	Returns the left child node
+      Arr[(2*i)+2]	Returns the right child node
+*/
+class MinHeap {
+
+}
