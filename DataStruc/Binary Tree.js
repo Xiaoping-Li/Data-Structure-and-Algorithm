52f00bd19ab5420e56bfaa7771d2a5155c9d4770
Xiaoping-Li <narci2008@gmail.com>
2019-03-28 15:56:48 -0700

    Update Binary Tree.js

--- DataStruc/Binary Tree.js
+++ DataStruc/Binary Tree.js
@@ -1,24 +1,54 @@
-// Define tree Node
-function Node(data) {
-  this.data = data;
-  this.left = null;
-  this.right = null;
-}
-
-// Define Binary Tree class
 class BinaryTree {
-  constructor() {
-    this.root = null;
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insertLeft(val) {
+    const newTree = new BinaryTree(val);
+    if (!this.left) {
+      this.left = newTree;
+    } else {
+      this.left.insertLeft(val);
+    }
+  }
+
+  insertRight(val) {
+    const newTree = new BinaryTree(val);
+    if (!this.right) {
+      this.right = newTree;
+    } else {
+      this.right.insertRight(val);
+    }
   }
   
-  insert(data) {
-    
+  preOrderTraversal() {
+    console.log(this.value);
+    if (this.left) this.left.preOrder();
+    if (this.right) this.right.preOrder();
   }
   
-  inOrderTraversal() {}
+  inOrderTraversal() {
+    if (this.left) this.left.inOrderTraversal();
+    console.log(this.value);
+    if (this.right) this.right.inOrderTraversal();
+  }
+
+  postOrderTraversal() {
+    if (this.left) this.left.postOrderTraversal();
+    if (this.right) this.right.postOrderTraversal();
+    console.log(this.value);
+  }
   
-  preOrderTraversal() {}
-  
-  postOrderTraversal() {}
-  
+  BFSTraversal() {
+    const queue = [];
+    queue.push(this);
+    while (queue.length) {
+      let current = queue.shift();
+      if (current.left) queue.push(current.left);
+      if (current.right) queue.push(current.right);
+      console.log(current.value);
+    }
+  }
 }
