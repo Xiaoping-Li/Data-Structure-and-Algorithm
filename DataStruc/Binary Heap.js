21dfca8626d80422c84103b222cd9ff2507e8d3b
Xiaoping-Li <narci2008@gmail.com>
2019-03-28 17:01:29 -0700

    Update Binary Heap.js

--- DataStruc/Binary Heap.js
+++ DataStruc/Binary Heap.js
@@ -5,7 +5,27 @@ Binary Heap
       Arr[(i-1)/2]	Returns the parent node
       Arr[(2*i)+1]	Returns the left child node
       Arr[(2*i)+2]	Returns the right child node
+      
+A heap is a useful and efficient way to store and look up data that must maintain order. The classic example is a priority 
+queue abstract data type. A priority queue is a set of data where higher or lower valued data points bubble to the front of the
+queue and are therefore accessed first. 
 */
 class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  insert(key) {
+    this.heap.push(key);
+    let i = this.heap.length - 1;
+    let parent = this.heap[Math.floor((i-1)/2)];
+    while (parent && key < parent) {
+      this.heap[i] = parent;
+      this.heap[Math.floor((i-1)/2)] = key;
+      i = Math.floor((i-1)/2);
+      parent = this.heap[Math.floor((i-1)/2)];
+    }
+    return key;
+  }
 
 }
