414ae833e754ff86538476a6a4877706879f528d
Xiaoping-Li <narci2008@gmail.com>
2019-04-01 10:05:06 -0700

    Update Is Unique.js

--- Cracking Interview/Chapter 1/Is Unique.js
+++ Cracking Interview/Chapter 1/Is Unique.js
@@ -6,6 +6,9 @@ Implement an algorithm to determine if a string has all unique characters. What 
 // Brute force
 const isUnique = (str) => {
   if (str.length === 1) return true;
+  // ASCII totally has 128 characters
+  // Extended ASCII has 256 characters, clarify with interviewer
+  if (str.length > 128) return false;
   for (let i = 0; i < str.length - 1; i++) {
     for (let j = i + 1; j < str.length; j++) {
       if (str[i] === str[j]) return false;
