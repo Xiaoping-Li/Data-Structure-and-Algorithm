233cc1c4984539f16e576b77037ef4592e371c15
Xiaoping-Li <narci2008@gmail.com>
2019-04-01 14:51:44 -0700

    Create 1.7 Rotate Matrix.js

---
+++ Cracking Interview/Chapter 1/1.7 Rotate Matrix.js
@@ -0,0 +1,13 @@
+/*
+1.7 Rotate Matrix
+
+Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes. Write a method to rotate the image by 90
+degree. Can you do this in place?
+
+Ask questions:
+1. Rotate direction: clockwise or anticlockwise?            
+*/
+
+const RotateMatrix = array => {
+  
+}
