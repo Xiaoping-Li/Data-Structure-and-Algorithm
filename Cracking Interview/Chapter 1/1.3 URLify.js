26765b8b92e28a6448626d2897eb0bfc627ab14c
Xiaoping-Li <narci2008@gmail.com>
2019-04-01 10:40:47 -0700

    Create 1.3 URLify.js

---
+++ Cracking Interview/Chapter 1/1.3 URLify.js
@@ -0,0 +1,17 @@
+/*
+1.3 URLify
+
+Write a method to replace all spaces in a string with '%20'. You may assume that the string has sufficient space at the end to
+hold the additional characters, and that you are given the true length of the string.
+Example: 
+Input:  'Mr John Smith     ', 13
+Output: 'Mr%20John%20Smith'
+
+String.trim()
+String.trimEnd()
+String.trimStart()
+*/
+
+const URLify = str => {
+  return str.trim().split(' ').join('%20');
+}
