6e8790358894269bd18d834995c671361fb4610c
Xiaoping-Li <narci2008@gmail.com>
2019-04-05 15:17:42 -0700

    Update 3.4 Queue via Stacks.js

--- Cracking Interview/Chapter 3/3.4 Queue via Stacks.js
+++ Cracking Interview/Chapter 3/3.4 Queue via Stacks.js
@@ -1,6 +1,16 @@
 /*
 3.4 Queue via Stacks
 Implement a MyQueue class which implements a queue using two stacks.
+
+Methods:
+add(item)
+remove()
+peek(): the top
+isEmpty()
+
+Note: 'StackNewest' has the newest elements on top and 'StackOldest' has the oldest elements on top. When we dequeue an 
+element,stackOldest.pop().If stackOldest is empty, then we transfer all elements from stackNewest into this stack in reversed
+order. To insert an element, stackNewest.push(), since it has the newest elements on top.
 */
 
 function StackNode(val) {
@@ -39,3 +49,50 @@ class Stack {
     return !this.top
   }
 }
+
+class MyQueue {
+  constructor() {
+    this.new = new Stack();
+    this.old = new Stack();
+  }
+  
+  isEmpty() {
+    return this.new.isEmpty && this.old.isEmpty;
+  }
+  
+  add(item) {
+    this.new.push(item);
+  }
+  
+  shiftStacks() {
+    if (this.old.isEmpty()) {
+      while(!this.new.isEmpty()) {
+        this.old.push(this.new.pop());
+      }
+    }
+  }
+  
+  remove() {
+    this.shiftStacks();
+    return this.old.pop();
+  }
+  
+  peek() {
+    this.shiftStacks();
+    return this.old.peek();
+  }  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
