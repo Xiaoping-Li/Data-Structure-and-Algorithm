345da6c17906800f8e7cf2920327e05ff9e36915
Xiaoping-Li <narci2008@gmail.com>
2019-04-05 09:28:42 -0700

    Update Queue.js

--- DataStruc/Queue.js
+++ DataStruc/Queue.js
@@ -2,45 +2,52 @@
 Queue: first-in-first-out. Element is added to the end, and removed from the head.
 Using array: enqueue --> O(1); dequeue --> O(n)
 Using linked list: enqueue --> O(n); dequeue --> O(1)
+
+Note: One place where queues are often used is in breadth-first search or in implementing a cache.
 */
+
+function Node(val) {
+  this.value = val;
+  this.next = null;
+};
 
 class Queue {
   constructor() {
     this.head = null;
-    this._length = 0;
+    this.tail = null;
   }
   
-  get size() {
-    return this._length;
-  }
-  
-  enqueue(value) {
-    const newNode = {
-      value,
-      next: null
-    };
-    
+  add(item) {
+    const newNode = new Node(item);
     if (!this.head) {
       this.head = newNode;
-      this._length++;
-      return newNode.value;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
-    
-    let tail = this.head;
-    while (tail.next) {
-      tail = tail.next;
-    }
-    tail.next = newNode;
-    this._length++;
-    return newNode.value;
+    return this.head;
   }
   
-  dequeue() {
-    if (!this.head) return null;
-    
-    let current = this.head;
-    this.head = current.next;
-    this._length--;
-    return current.value;
+  remove() {
+    if (!this.head) return 'Empty Queue';
+    const removeItem = this.head.value;
+    if (!this.head.next) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = this.head.next;
+    }
+    return removeItem;
+  }
+  
+  // Return the top of the queue
+  peek() {
+    if (!this.head) return 'Empty Queue';
+    return this.head.value;
+  }
+  
+  isEmpty() {
+    return !this.head;
   }
 }
