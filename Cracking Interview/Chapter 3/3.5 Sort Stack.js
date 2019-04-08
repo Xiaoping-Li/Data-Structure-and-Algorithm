0f3c9c1373854f912cd127c082f2b47d5060abb9
Xiaoping-Li <narci2008@gmail.com>
2019-04-08 09:30:57 -0700

    Create 3.5 Sort Stack.js

---
+++ Cracking Interview/Chapter 3/3.5 Sort Stack.js
@@ -0,0 +1,10 @@
+/*
+3.5 Sort Stack
+Write a program to sort a stack such that the smallest items are on the top. You can use an additional temporary stack, but you
+may not copy the elements into any other data structure (such as an array). The stack supports the following operations: push,
+pop, peek, and isEmpty.
+*/
+
+const sortStack = nums => {
+  const storage = new Stack();
+};
