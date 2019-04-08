3b009f4b2ae5d67848185e02c70a94f04142cc24
Xiaoping-Li <narci2008@gmail.com>
2019-04-08 09:57:37 -0700

    Update 3.5 Sort Stack.js

--- Cracking Interview/Chapter 3/3.5 Sort Stack.js
+++ Cracking Interview/Chapter 3/3.5 Sort Stack.js
@@ -5,6 +5,61 @@ may not copy the elements into any other data structure (such as an array). The 
 pop, peek, and isEmpty.
 */
 
+function StackNode(val) {
+  this.value = val;
+  this.next = null;
+};
+
+class Stack {
+  constructor() {
+    this.top = null;
+  }
+  
+  pop() {
+    if(!this.top) return 'Empty stack';
+    let popItem = this.top.value;
+    this.top = this.top.next;
+    return popItem;
+  }
+  
+  push(item) {
+    const newNode = new StackNode(item);
+    newNode.next = this.top;
+    this.top = newNode;
+    return this.top;
+  }
+  
+  peek() {
+    if (!this.top) return 'Empty stack';
+    return this.top.value;
+  }
+  
+  isEmpty() {
+    return !this.top
+  }
+}
+
 const sortStack = nums => {
   const storage = new Stack();
+  while (!nums.isEmpty()) {
+    let temp = nums.pop();
+    if (storage.isEmpty()) {
+      storage.push(temp);
+    } else {
+      if (temp >= storage.peek()) {
+        storage.push(temp);
+      } else {
+        while (temp < storage.peek() && !storage.isEmpty()) {
+          nums.push(storage.pop());
+        }
+        storage.push(temp);
+      }
+    }
+  }
+  
+  while (!storage.isEmpty()) {
+    nums.push(storage.pop());
+  } 
+
+  return nums;  
 };
